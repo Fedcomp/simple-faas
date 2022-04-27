f6f677dca0bf024005eb47571928370610aac155
Fedcomp <aglergen@gmail.com>
2022-04-27 04:41:56 +0300

    Call docker function as api

--- simple-faas/src/config.rs
+++ simple-faas/src/config.rs
@@ -1,14 +1,21 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Config {
     pub version: u64,
     pub docker_host: String,
+    #[serde(default = "default_listen_host")]
+    pub listen_host: SocketAddr,
     pub functions: HashMap<String, FunctionData>,
 }
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct FunctionData {
     pub image: String,
+}
+
+fn default_listen_host() -> SocketAddr {
+    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080)
 }
--- simple-faas/src/main.rs
+++ simple-faas/src/main.rs
@@ -1,10 +1,15 @@
+mod config;
+
 use env_logger::Env;
 use log::{debug, info};
-use simple_faas::config::Config;
+use self::config::Config;
+use std::fs::File;
+use warp::Filter;
+use std::sync::Arc;
 use simple_faas_docker::client::Client as DockerClient;
 use simple_faas_docker::v1_37::Api as DockerApi;
 use simple_faas_docker::v1_37::ContainerCreateArgs;
-use std::fs::File;
+use warp::http::Response;
 
 const DEFAULT_CONFIG_NAME: &str = "config.yml";
 
@@ -15,19 +20,56 @@ async fn main() -> anyhow::Result<()> {
 
     let config_reader = File::open(DEFAULT_CONFIG_NAME)?;
     let config: Config = serde_yaml::from_reader(config_reader)?;
+    let config = Arc::new(config);
+    let listen_host = config.listen_host;
     debug!("Loaded {} function(s) from config", config.functions.len());
 
-    let first_function = config.functions.iter().next().expect("No functions were loaded").1;
+    let config = warp::any().map(move || config.clone());
 
-    let client = DockerClient::new(config.docker_host);
+    let function_call_filter =
+        warp::path!("function" / String)
+        .and(config)
+        .and_then(function_call_handler);
+
+    info!("Listening on {:?}", listen_host);
+
+    warp::serve(function_call_filter)
+        .run(listen_host)
+        .await;
+
+    Ok(())
+}
+
+async fn function_call_handler(name: String, config: Arc<Config>) -> Result<impl warp::Reply, warp::Rejection> {
+    let response = match call_docker_function(name, config).await {
+        Ok(output) => {
+            Response::builder()
+                .status(200)
+                .body(output)
+                .expect("Failed to construct a response")
+        },
+        Err(e) => {
+            Response::builder()
+                .status(500)
+                .body(format!("Failed to call function: {}", e))
+                .expect("Failed to construct a response")
+        }
+    };
+
+    Ok(response)
+}
+
+async fn call_docker_function(name: String, config: Arc<Config>) -> anyhow::Result<String> {
+    let client = DockerClient::new(config.docker_host.clone());
     let api = DockerApi::new(client);
     let container_create_opts = ContainerCreateArgs {
-        Image: first_function.image.clone(),
+        Image: name.clone(),
         Cmd: None,
     };
     let container = api.containers().create(container_create_opts).await?;
     container.start().await?;
     container.wait().await?;
     container.delete().await?;
-    Ok(())
+
+    Ok("TODO: Output result".to_string())
 }
